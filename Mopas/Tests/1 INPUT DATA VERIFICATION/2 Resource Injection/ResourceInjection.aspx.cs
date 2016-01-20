5d3e5eb66f66d9f0bb6c03adba863bb1a697203f
SDL Demo User <msharonov@ptsecurity.ru>
2016-01-20 15:37:48 +0300

    [AFD] in [ResourceInjection.aspx.cs]

--- Mopas/Tests/1 INPUT DATA VERIFICATION/2 Resource Injection/ResourceInjection.aspx.cs
+++ Mopas/Tests/1 INPUT DATA VERIFICATION/2 Resource Injection/ResourceInjection.aspx.cs
@@ -3,15 +3,21 @@ using System.IO;
 
 namespace Mopas.Tests
 {
+    /// <summary>
+    /// 2.
+    /// Resource Injection 
+    /// MOPAS
+    /// Contains 1 vulnerability
+    /// </summary>
     public partial class ResourceInjection : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
             string fileName = Request.Params["report"];
 
-            if (fileName == null)
+            if (fileName != null)
             {
-                // FIXED: AI issue #2, High, Arbitrary File Deletion, https://github.com/sdldemo/MOPAS/issues/2
+                // TODO: AI issue #4, High, Arbitrary File Deletion, https://github.com/sdldemo/MOPAS/issues/4
                 // GET /Tests/1 INPUT DATA VERIFICATION/2 Resource Injection/ResourceInjection.aspx?report=Default.aspx HTTP/1.1
                 // Host:localhost
                 File.Delete("D:\\AI\\Reports\\" + fileName);
