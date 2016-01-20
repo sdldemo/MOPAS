dd27e0f53668e1ae01dc9e0302ceeba75d0ffdb0
SDL Demo User <msharonov@ptsecurity.ru>
2016-01-20 15:37:46 +0300

    [SQL] in [Sqli.aspx.cs]

--- Mopas/Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx.cs
+++ Mopas/Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Data.SqlClient;
 
 namespace Mopas.Tests
@@ -14,31 +14,26 @@ namespace Mopas.Tests
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-                int id = Convert.ToInt32(Request.Params["id"]);
+            string id = Request.Params["id"];
 
-                int str1 = 0;
+            string str1 = "";
 
-                using (
-                    var connection =
-                        new SqlConnection(
-                            System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"]
-                                .ConnectionString))
-                using (var command = connection.CreateCommand())
+            using (var connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+            using (var command = connection.CreateCommand())
+            {
+                // TODO: AI issue #3, High, SQL Injection, https://github.com/sdldemo/MOPAS/issues/3
+                // GET /Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx?id=1%27+AND+%271%27%3d%272 HTTP/1.1
+                // Host:localhost
+                command.CommandText = "SELECT test FROM news where id=" + id;
+                connection.Open();
+                using (var reader = command.ExecuteReader())
                 {
-                    // FP: AI issue #1, High, SQL Injection, https://github.com/sdldemo/MOPAS/issues/1
-                    // GET /Tests/1 INPUT DATA VERIFICATION/1 SQL Injection/Sqli.aspx HTTP/1.1
-                    // Host:localhost
-                    // (System.Convert.ToInt32(this.Request.Params["id"]) == "1' AND '1'='2")
-                    command.CommandText = string.Format("SELECT test FROM news where id={0}", id);
-                    connection.Open();
-                    using (var reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                            str1 += (int) reader["ColumnName"];
-                    }
+                    while (reader.Read())
+                        str1 += reader["ColumnName"].ToString();
                 }
-
-                Response.Write("<b>" + str1 + "</b>");
             }
+
+            Response.Write("<b>" + str1 + "</b>");
         }
     }
+}
